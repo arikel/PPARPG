838cde417e09e0d2c1bd7630903d1d56c8bce5ee
arikel <arikel@arikel-desktop.(none)>
2011-12-23 13:59:43 +0100

    bugtest file

--- bugTest.py
+++ bugTest.py
@@ -21,6 +21,8 @@ class NPC:
 			self.seq.pause()
 		for i in range(5):
 			self.seq.append(Func(self.lookAt, Point3(0,i,i)))
+			pos_i = LerpPosInterval(self.model,	5,(i, -i, 0))
+			self.seq.append(pos_i)
 		self.seq.loop()
 		
 for i in range(5):
